abae61cbf497f9db6b2860537ef88b335589f07f
Jenny Thorne <jennythorne3006@gmail.com>
2015-10-04 22:21:26 +0100

    Making adafruit less crap

--- catnet/adafruit/i2c.py
+++ catnet/adafruit/i2c.py
@@ -53,109 +53,69 @@ class Adafruit_I2C(object):
       data >>= 8
     return val
 
-  def errMsg(self):
-    print "Error accessing 0x%02X: Check your I2C address" % self.address
-    return -1
-
   def write8(self, reg, value):
     "Writes an 8-bit value to the specified register/address"
-    try:
-      self.bus.write_byte_data(self.address, reg, value)
-      if self.debug:
-        print "I2C: Wrote 0x%02X to register 0x%02X" % (value, reg)
-    except IOError, err:
-      return self.errMsg()
+    self.bus.write_byte_data(self.address, reg, value)
+    if self.debug:
+      print "I2C: Wrote 0x%02X to register 0x%02X" % (value, reg)
 
   def write16(self, reg, value):
     "Writes a 16-bit value to the specified register/address pair"
-    try:
-      self.bus.write_word_data(self.address, reg, value)
-      if self.debug:
-        print ("I2C: Wrote 0x%02X to register pair 0x%02X,0x%02X" %
-         (value, reg, reg+1))
-    except IOError, err:
-      return self.errMsg()
+    self.bus.write_word_data(self.address, reg, value)
+    if self.debug:
+      print ("I2C: Wrote 0x%02X to register pair 0x%02X,0x%02X" % (value, reg, reg+1))
 
   def writeRaw8(self, value):
     "Writes an 8-bit value on the bus"
-    try:
-      self.bus.write_byte(self.address, value)
-      if self.debug:
-        print "I2C: Wrote 0x%02X" % value
-    except IOError, err:
-      return self.errMsg()
+    self.bus.write_byte(self.address, value)
+    if self.debug:
+      print "I2C: Wrote 0x%02X" % value
 
   def writeList(self, reg, list):
     "Writes an array of bytes using I2C format"
-    try:
-      if self.debug:
-        print "I2C: Writing list to register 0x%02X:" % reg
-        print list
-      self.bus.write_i2c_block_data(self.address, reg, list)
-    except IOError, err:
-      return self.errMsg()
+    if self.debug:
+      print "I2C: Writing list to register 0x%02X:" % reg
+      print list
+    self.bus.write_i2c_block_data(self.address, reg, list)
 
   def readList(self, reg, length):
     "Read a list of bytes from the I2C device"
-    try:
-      results = self.bus.read_i2c_block_data(self.address, reg, length)
-      if self.debug:
-        print ("I2C: Device 0x%02X returned the following from reg 0x%02X" %
-         (self.address, reg))
-        print results
-      return results
-    except IOError, err:
-      return self.errMsg()
+    results = self.bus.read_i2c_block_data(self.address, reg, length)
+    if self.debug:
+      print ("I2C: Device 0x%02X returned the following from reg 0x%02X" % (self.address, reg))
+      print results
+    return results
 
   def readU8(self, reg):
     "Read an unsigned byte from the I2C device"
-    try:
-      result = self.bus.read_byte_data(self.address, reg)
-      if self.debug:
-        print ("I2C: Device 0x%02X returned 0x%02X from reg 0x%02X" %
-         (self.address, result & 0xFF, reg))
-      return result
-    except IOError, err:
-      return self.errMsg()
+    result = self.bus.read_byte_data(self.address, reg)
+    if self.debug:
+      print ("I2C: Device 0x%02X returned 0x%02X from reg 0x%02X" % (self.address, result & 0xFF, reg))
+    return result
 
   def readS8(self, reg):
     "Reads a signed byte from the I2C device"
-    try:
-      result = self.bus.read_byte_data(self.address, reg)
-      if result > 127: result -= 256
-      if self.debug:
-        print ("I2C: Device 0x%02X returned 0x%02X from reg 0x%02X" %
-         (self.address, result & 0xFF, reg))
-      return result
-    except IOError, err:
-      return self.errMsg()
+    result = self.bus.read_byte_data(self.address, reg)
+    if result > 127:
+      result -= 256
+    if self.debug:
+      print ("I2C: Device 0x%02X returned 0x%02X from reg 0x%02X" % (self.address, result & 0xFF, reg))
+    return result
 
   def readU16(self, reg, little_endian=True):
     "Reads an unsigned 16-bit value from the I2C device"
-    try:
-      result = self.bus.read_word_data(self.address,reg)
-      # Swap bytes if using big endian because read_word_data assumes little
-      # endian on ARM (little endian) systems.
-      if not little_endian:
-        result = ((result << 8) & 0xFF00) + (result >> 8)
-      if (self.debug):
-        print "I2C: Device 0x%02X returned 0x%04X from reg 0x%02X" % (self.address, result & 0xFFFF, reg)
-      return result
-    except IOError, err:
-      return self.errMsg()
+    result = self.bus.read_word_data(self.address,reg)
+    # Swap bytes if using big endian because read_word_data assumes little
+    # endian on ARM (little endian) systems.
+    if not little_endian:
+      result = ((result << 8) & 0xFF00) + (result >> 8)
+    if (self.debug):
+      print "I2C: Device 0x%02X returned 0x%04X from reg 0x%02X" % (self.address, result & 0xFFFF, reg)
+    return result
 
   def readS16(self, reg, little_endian=True):
     "Reads a signed 16-bit value from the I2C device"
-    try:
-      result = self.readU16(reg,little_endian)
-      if result > 32767: result -= 65536
-      return result
-    except IOError, err:
-      return self.errMsg()
-
-if __name__ == '__main__':
-  try:
-    bus = Adafruit_I2C(address=0)
-    print "Default I2C bus is accessible"
-  except:
-    print "Error accessing default I2C bus"
+    result = self.readU16(reg,little_endian)
+    if result > 32767:
+      result -= 65536
+    return result
