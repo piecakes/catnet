db2b4ff617822ce52d434b32f5d63ddd7b5b37b5
Jenny Thorne <jennythorne3006@gmail.com>
2015-10-04 17:21:01 +0100

    Using new adafruit compas library

--- catnet/compass.py
+++ catnet/compass.py
@@ -1,50 +1,29 @@
-import smbus
 import math
+from Adafruit_I2C import Adafruit_I2C
 
 
-bus = smbus.SMBus(1)
-address = 0x1e
+LSM303_ADDRESS_MAG   = (0x3C >> 1)  # 0011110x
+LSM303_REGISTER_MAG_MR_REG_M = 0x02
+LSM303_REGISTER_MAG_OUT_X_H_M = 0x03
 
-def read_byte(adr):
-    return bus.read_byte_data(address, adr)
+mag   = Adafruit_I2C(LSM303_ADDRESS_MAG, 1, False)
+mag.write8(LSM303_REGISTER_MAG_MR_REG_M, 0x00)
 
-def read_word(adr):
-    high = bus.read_byte_data(address, adr)
-    low = bus.read_byte_data(address, adr+1)
-    val = (high << 8) + low
-    return val
 
-def read_word_2c(adr):
-    val = read_word(adr)
-    if (val >= 0x8000):
-        return -((65535 - val) + 1)
-    else:
-        return val
-
-def write_byte(adr, value):
-    bus.write_byte_data(address, adr, value)
-
-write_byte(0, 0b01110000) # Set to 8 samples @ 15Hz
-write_byte(1, 0b00100000) # 1.3 gain LSb / Gauss 1090 (default)
-write_byte(2, 0b00000000) # Continuous sampling
-
-# write_byte(0, 0b11110000)
-# write_byte(1, 0b01100000)
-# write_byte(2, 0b00000000)
-
-scale = 0.92
-
-x_offset = 28
-y_offset = 0
+# Interpret signed 16-bit magnetometer component from list
+def mag_16(buf, idx):
+    n = (buf[idx] << 8) | buf[idx+1]   # High, low bytes
+    return n if n < 32768 else n - 65536 # 2's complement signed
 
 
 def get_bearing():
-
-    x_out = (read_word_2c(3) - x_offset) * scale
-    y_out = (read_word_2c(7) - y_offset) * scale
-
-    bearing  = math.atan2(y_out, x_out)
+    scale = 0.92
+    x_offset = 28
+    y_offset = 0
+    buf = mag.readList(LSM303_REGISTER_MAG_OUT_X_H_M, 6)
+    x = (mag_16(buf, 0) - x_offset) * scale
+    y = (mag_16(buf, 4) - y_offset) * scale
+    bearing  = math.atan2(y, x)
     if (bearing < 0):
         bearing += 2 * math.pi
-
     return math.degrees(bearing)
